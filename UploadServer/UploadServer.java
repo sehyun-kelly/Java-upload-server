fc941fb5e0b04d81e0566819c98f10dcab7f018a
Kelly Park <sehyun.kelly.park@gmail.com>
2022-10-07 04:46:03 -0700

    UploadServer/UploadServerThread multithreading completed

--- UploadServer/UploadServer.java
+++ UploadServer/UploadServer.java
@@ -2,44 +2,25 @@ import java.net.*;
 import java.io.*;
 
 public class UploadServer {
+    private static int count;
+    private static ServerSocket serverSocket;
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
+        count = 0;
+        System.out.println("starting server");
         try {
             serverSocket = new ServerSocket(8080);
-            Socket clientSocket = serverSocket.accept();
-            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-//            InputStream inputStream = clientSocket.getInputStream();
-//            DataInputStream dataInputStream = new DataInputStream(inputStream);
-//            String message = dataInputStream.readUTF();
-//            System.out.println("Title: "+message);
 
-            out.writeBytes("HTTP/1.1 200 OK\r\n");
-            out.writeBytes("Content-Type: text/html\r\n\r\n");
-            String htmlPage = "<!DOCTYPE html>" +
-                    "<html><head><title>File Upload Form</title></head>" +
-                    "<body><h1>Upload file</h1>" +
-                    "<form method=\"POST\" action=\"upload\" " +
-                    "enctype=\"multipart/form-data\">" +
-                    "<input type=\"file\" name=\"fileName\"/><br/><br/>" +
-                    "Caption: <input type=\"text\" name=\"caption\"<br/><br/>" +
-                    "<br />" +
-                    "Date: <input type=\"date\" name=\"date\"<br/><br/>" +
-                    "<br />" +
-                    "<input type=\"submit\" value=\"Submit\"/>" +
-                    "</form>" +
-                    "</body></html>";
-            out.writeBytes(htmlPage);
-
-//            clientSocket.close();
-//            serverSocket.close();
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                UploadServerThread uploadServerThread = new UploadServerThread(clientSocket, count);
+                uploadServerThread.start();
+                count++;
+            }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port 8080 or listening for a connection");
             System.out.println(e.getMessage());
             System.exit(-1);
         }
 
-        while (true) {
-            new UploadServerThread(serverSocket.accept()).start();
-        }
     }
 }
--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -3,26 +3,59 @@ import java.io.*;
 import java.time.Clock;
 
 public class UploadServerThread extends Thread {
-    private Socket socket = null;
-
-    public UploadServerThread(Socket socket) {
+    private Socket socket;
+    private int connectionCount;
+    public UploadServerThread(Socket socket, int count) {
         super("DirServerThread");
         this.socket = socket;
+        this.connectionCount = count;
     }
 
     public void run() {
         try {
-            InputStream in = socket.getInputStream();
-            HttpServletRequest req = new HttpServletRequest(in);
-            OutputStream baos = new ByteArrayOutputStream();
-            HttpServletResponse res = new HttpServletResponse(baos);
-            HttpServlet httpServlet = new UploadServlet();
-            httpServlet.doPost(req, res);
-            OutputStream out = socket.getOutputStream();
-            out.write(((ByteArrayOutputStream) baos).toByteArray());
-            socket.close();
+            if(socket.getLocalAddress().toString().contains("127")){
+                System.out.println("Client " + connectionCount + " from console");
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+                HttpServletRequest req = new HttpServletRequest(in);
+                OutputStream baos = new ByteArrayOutputStream();
+                HttpServletResponse res = new HttpServletResponse(baos);
+                HttpServlet httpServlet = new UploadServlet();
+                httpServlet.doPost(req, res);
+                out.write(((ByteArrayOutputStream) baos).toByteArray());
+            }else{
+                System.out.println("Client " + connectionCount + " from web");
+                OutputStream out = socket.getOutputStream();
+                String htmlPage = "<!DOCTYPE html>" +
+                        "<html><head><title>File Upload Form</title></head>" +
+                        "<body><h1>Upload file</h1>" +
+                        "<form method=\"POST\" action=\"upload\" " +
+                        "enctype=\"multipart/form-data\">" +
+                        "<input type=\"file\" name=\"fileName\"/><br/><br/>" +
+                        "Caption: <input type=\"text\" name=\"caption\"<br/><br/>" +
+                        "<br />" +
+                        "Date: <input type=\"date\" name=\"date\"<br/><br/>" +
+                        "<br />" +
+                        "<input type=\"submit\" value=\"Submit\"/>" +
+                        "</form>" +
+                        "</body></html>";
+
+                final String CRLF = "\r\n";
+
+                String response = "HTTP/1.1 200 OK" + CRLF +
+                        "Content-Length: " + htmlPage.getBytes().length + CRLF + CRLF
+                        + htmlPage + CRLF + CRLF;
+
+                out.write(response.getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if(socket != null){
+                try {
+                    socket.close();
+                }catch (IOException e){}
+            }
         }
     }
 }
