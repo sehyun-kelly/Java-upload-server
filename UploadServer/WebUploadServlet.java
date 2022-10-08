dfd5ede112cd7d44619a0ee5db4d8cddbe31aacc
Melanie Ta <nngoc2701@gmail.com>
2022-10-08 12:35:17 -0700

    Show all images when upload from web

--- UploadServer/WebUploadServlet.java
+++ UploadServer/WebUploadServlet.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class WebUploadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -24,21 +28,34 @@ public class WebUploadServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        Path currentRelativePath = Paths.get("");
         response.htmlPage = "<!DOCTYPE html>\r\n" +
                 "<html>\n" +
                 "   <head>\n" +
-                "       <title>File Upload Form</title>\n" +
+                "       <title>File Upload Succeed</title>\n" +
                 "   </head>\n" +
                 "   <body>\n" +
                 "       <p>Upload succeeded!</p><br/>\n" +
-                "       <h1>Upload file</h1>\n" +
-                "       <form method=\"POST\" action=\"upload\" enctype=\"multipart/form-data\">\n" +
-                "           <input type=\"file\" name=\"fileName\"/><br/><br/>\n" +
-                "           Caption: <input type=\"text\" name=\"caption\"<br/><br/><br/>\n" +
-                "           Date: <input type=\"date\" name=\"date\"<br/><br/><br/>\n" +
-                "           <input type=\"submit\" value=\"Submit\"/>\n" +
+                "       <div><ul>" + getListing(currentRelativePath.toAbsolutePath().getParent() + "/images/")
+                + "</ul></div>\n" +
+                "       <form method=\"GET\" action=\"\">\n" +
+                "           <input type=\"submit\" value=\"back\"/>\n" +
                 "       </form>\n" +
                 "   </body>\n" +
                 "</html>";
     }
+
+    private String getListing(String path) {
+        File dir = new File(path);
+        String[] chld = dir.list();
+
+        if (chld == null) return "No files found";
+
+        String dirList = "All images:";
+        for (String s : chld) {
+            dirList += "<li>" + s + "</li>";
+        }
+        return dirList;
+    }
+
 }
