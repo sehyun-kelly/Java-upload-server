20308a0acd2686319897b5f43c0ecb570b010b05
Kelly Park <89621420+sehyun-kelly@users.noreply.github.com>
2023-06-27 17:44:42 -0700

    Update WebUploadServlet to add style

--- UploadServer/WebUploadServlet.java
+++ UploadServer/WebUploadServlet.java
@@ -17,13 +17,47 @@ public class WebUploadServlet extends HttpServlet {
                     "<html>\n" +
                     "   <head>\n" +
                     "       <title>File Upload Form</title>\n" +
+                    "<style>\n" +
+                    "            body {\n" +
+                    "                font-family: Arial, sans-serif;\n" +
+                    "                background-color: #f4f4f4;\n" +
+                    "                padding: 20px;\n" +
+                    "            }\n" +
+                    "            h1 {\n" +
+                    "                color: #333;\n" +
+                    "            }\n" +
+                    "            form {\n" +
+                    "                background: #fff;\n" +
+                    "                padding: 20px;\n" +
+                    "                border-radius: 8px;\n" +
+                    "                box-shadow: 0 0 10px rgba(0,0,0,0.1);\n" +
+                    "            }\n" +
+                    "            input[type=\"text\"], input[type=\"date\"], input[type=\"file\"] {\n" +
+                    "                width: 95%;\n" +
+                    "                padding: 10px;\n" +
+                    "                border-radius: 4px;\n" +
+                    "                border: 1px solid #ccc;\n" +
+                    "                margin-bottom: 10px;\n" +
+                    "            }\n" +
+                    "            input[type=\"submit\"] {\n" +
+                    "                background: #33c;\n" +
+                    "                color: #fff;\n" +
+                    "                border: none;\n" +
+                    "                padding: 10px 20px;\n" +
+                    "                border-radius: 4px;\n" +
+                    "                cursor: pointer;\n" +
+                    "            }\n" +
+                    "            input[type=\"submit\"]:hover {\n" +
+                    "                background: #339;\n" +
+                    "            }\n" +
+                    "        </style>"+
                     "   </head>\n" +
                     "   <body>\n" +
                     "       <h1>Upload file</h1>\n" +
                     "       <form method=\"POST\" action=\"upload\" enctype=\"multipart/form-data\">\n" +
                     "           <input type=\"file\" name=\"fileName\"/><br/><br/>\n" +
-                    "           Caption: <input type=\"text\" name=\"caption\"<br/><br/><br/>\n" +
-                    "           Date: <input type=\"date\" name=\"date\"<br/><br/><br/>\n" +
+                    "           Caption: <br/><input type=\"text\" name=\"caption\"><br/><br/>\n" +
+                    "           Date: <br/><input type=\"date\" name=\"date\"><br/><br/>\n" +
                     "           <input type=\"submit\" value=\"Submit\"/>\n" +
                     "       </form>\n" +
                     "   </body>\n" +
@@ -48,9 +82,45 @@ public class WebUploadServlet extends HttpServlet {
                 "<html>\n" +
                 "   <head>\n" +
                 "       <title>File Upload Succeed</title>\n" +
+                "<style>\n" +
+                "            body {\n" +
+                "                font-family: Arial, sans-serif;\n" +
+                "                background-color: #f4f4f4;\n" +
+                "                padding: 20px;\n" +
+                "            }\n" +
+                "            h1 {\n" +
+                "                color: #4CAF50;\n" +
+                "            }\n" +
+                "            div {\n" +
+                "                background: #fff;\n" +
+                "                padding: 20px;\n" +
+                "                border-radius: 8px;\n" +
+                "                box-shadow: 0 0 10px rgba(0,0,0,0.1);\n" +
+                "                margin-top: 20px;\n" +
+                "            }\n" +
+                "            ul {\n" +
+                "                list-style-type: none;\n" +
+                "                padding: 0;\n" +
+                "            }\n" +
+                "            li {\n" +
+                "                margin-bottom: 5px;\n"+
+                "            }\n" +
+                "            input[type=\"submit\"] {\n" +
+                "                background: #33c;\n" +
+                "                color: #fff;\n" +
+                "                border: none;\n" +
+                "                padding: 10px 20px;\n" +
+                "                border-radius: 4px;\n" +
+                "                cursor: pointer;\n" +
+                "                margin-top: 20px;\n" +
+                "            }\n" +
+                "            input[type=\"submit\"]:hover {\n" +
+                "                background: #339;\n" +
+                "            }\n" +
+                "        </style>\n"+
                 "   </head>\n" +
                 "   <body>\n" +
-                "       <p>Upload succeeded!</p><br/>\n" +
+                "       <h1>Upload succeeded!</h1><br/>\n" +
                 "       <div><ul>\n" + getListing(currentRelativePath.toAbsolutePath() + "/images/") +
                 "       </ul></div>\n" +
                 "       <form method=\"GET\" action=\"\">\n" +
@@ -67,7 +137,7 @@ public class WebUploadServlet extends HttpServlet {
         if (child == null) return "No files found";
 
         Arrays.sort(child);
-        String dirList = "All images:";
+        String dirList = "<h3>Images uploaded: </h3>";
         for (String s : child) {
             dirList += "<li>" + s + "</li>";
         }
